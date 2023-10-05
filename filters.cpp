5c65e20a273ebc0da5ed486a9f11ad368d342a7d
Sayed Reda <sydr38734@gmail.com>
2023-10-05 11:33:31 +0200

    Added first 6 filters and options window

---
+++ filters.cpp
@@ -0,0 +1,131 @@
+
+// Editor Function Declaration
+#include <iostream>
+#include <cstring>
+#include "bmplib.cpp"
+
+
+// Black&White _________________________________________
+void turnBW(unsigned char imageMatrix[SIZE][SIZE])
+{
+	for (int i = 0; i < SIZE; i++)
+	{
+		for (int j = 0; j < SIZE; j++)
+		{
+			if (imageMatrix[i][j] > 127)
+				imageMatrix[i][j] = 255;
+			else
+				imageMatrix[i][j] = 0;
+		}
+	}
+}
+
+// Flilp _____________________________________________
+void horizontalFlip(unsigned char imageMatrix[SIZE][SIZE])
+{
+	for (int i = 0; i < SIZE; ++i)
+	{
+		int l = 0, r = SIZE - 1;
+		while (l < r)
+		{
+			std::swap(imageMatrix[i][l++], imageMatrix[i][r--]);
+		}
+	}
+}
+
+void verticalFlip(unsigned char imageMatrix[SIZE][SIZE])
+{
+	for (int col = 0; col < SIZE; ++col)
+	{
+		int upRow = 0, downRow = SIZE - 1;
+		while (upRow < downRow)
+		{
+			std::swap(imageMatrix[upRow++][col], imageMatrix[downRow--][col]);
+		}
+	}
+}
+
+void flipFilter(unsigned char imageMatrix[SIZE][SIZE]) {
+
+}
+
+// Inverter _____________________________________________
+void invertFilter(unsigned char imageMatrix[SIZE][SIZE])
+{
+	for (int i = 0; i < SIZE; i++)
+	{
+		for (int j = 0; j < SIZE; j++)
+		{
+			imageMatrix[i][j] = 255 - imageMatrix[i][j];
+		}
+	}
+}
+
+
+// Merge Images _____________________________________________
+void mergeImages(unsigned char imageMatrix[SIZE][SIZE])
+{
+	char otherImageName[100];
+	unsigned char otherMatrix[SIZE][SIZE];
+
+	// Take the other image name
+	std::cout << "  Enter image name to merge with: ";
+	std::cin >> otherImageName;
+
+	// add .bmp to it
+	strcat(otherImageName, ".bmp");
+	readGSBMP(otherImageName, otherMatrix);
+
+	// Merging
+	for (int i = 0; i < SIZE; ++i) {
+		for (int j = 0; j < SIZE; ++j) {
+			imageMatrix[i][j] = (otherMatrix[i][j] + imageMatrix[i][j]) / 2;
+		}
+	}
+
+}
+
+// Rotate _____________________________________________
+void rotateRight(unsigned char imageMatrix[SIZE][SIZE])
+{
+	unsigned char tempImage[SIZE][SIZE];
+
+	// for each row in main image
+	for (int i = 0, tempCol = SIZE; i < SIZE; ++i, --tempCol)
+		// copy each row in image to be last column in newImage
+		for (int j = 0; j < SIZE; ++j)
+			tempImage[j][tempCol] = imageMatrix[i][j];
+
+	// Copying new image to main image
+	for (int i = 0; i < SIZE; ++i)
+		for (int j = 0; j < SIZE; ++j)
+			imageMatrix[i][j] = tempImage[i][j];
+}
+
+void rotateImage(unsigned char imageMatrix[SIZE][SIZE]) {
+	int degree;
+	std::cout << "  Rotate 90, 180, 270, or 360? ";
+	std::cin >> degree;
+
+	// Diving by 90 to get exact number of right rotations
+	degree /= 90;
+
+	while (degree--) rotateRight(imageMatrix);
+}
+
+// Brightness filter _____________________________________
+void brightnessFilter(unsigned char imageMatrix[SIZE][SIZE]) {
+	char option;
+	std::cout << "  (D)arken or (L)ighten Image? ";
+	std::cin >> option;
+
+	// Deciding what degree of lightening and darkening
+	int degree = (option == 'D' ? -50 : 50);
+
+	// Add degree of brightness to each pixel
+	for (int i = 0; i < SIZE; ++i) {
+		for (int j = 0; j < SIZE; ++j) {
+			imageMatrix[i][j] = std::max(0, std::min(255, imageMatrix[i][j] + degree));
+		}
+	}
+}
---
+++ main.cpp
@@ -0,0 +1,117 @@
+
+// FCAI - OOP Programming - 2023 - Assignment 1
+// Program Name:			Grayscale Photo Editor
+// Last Modified Date:		4/10/2023
+// Author1:					Elsayed Reda		(20220066)
+// Author2:					Hesham Abdelnaser	(20220373)
+// TA:						XX
+// Purpose:					Learning Problem solving and how to manage projects
+
+
+#include "filters.cpp"	// has everything
+
+
+unsigned char mainImageMatrix[SIZE][SIZE];
+
+void loadImage();
+void saveImage();
+char optionsDisplay();
+
+
+
+int main() {
+	std::cout << "User Beeh Aamel Eeh ^_^\n\n";
+	loadImage();
+
+	char option;
+	do {
+		option = optionsDisplay();
+		switch (option) {
+			case '1': {
+				turnBW(mainImageMatrix);
+				break;
+			}
+			case '2': {
+				invertFilter(mainImageMatrix);
+				break;
+			}
+			case '3': {
+				mergeImages(mainImageMatrix);
+				break;
+			}
+			case '4': {
+				flipFilter(mainImageMatrix);
+				break;
+			}
+			case '5': {
+				brightnessFilter(mainImageMatrix);
+				break;
+			}
+			case '6': {
+				rotateImage(mainImageMatrix);
+				break;
+			}
+			case 's': {
+				saveImage();
+				break;
+			}
+			case '0': {
+				std::cout << "Byee :)" << std::endl;
+			}
+		}
+	} while (option - '0');
+
+	return 0;
+}
+
+
+
+
+// ------------------- Functions Declaration ------------------------
+void saveImage() {
+	char targetName[100];
+
+	std::cout << "  Please enter target name: ";
+	std::cin >> targetName;
+
+	// adding bitmap extension
+	strcat(targetName, ".bmp");
+	writeGSBMP(targetName, mainImageMatrix);
+}
+
+void loadImage() {
+	char imageName[100];
+
+	std::cout << "Please enter image name: ";
+	std::cin >> imageName;
+
+	// adding bitmap extension
+	strcat(imageName, ".bmp");
+	readGSBMP(imageName, mainImageMatrix);
+}
+
+char optionsDisplay() {
+	std::cout << "Select your filter number [0 to exit]: " << std::endl;
+	std::cout << "  1) Black & White Filter" << std::endl;
+	std::cout << "  2) Invert Filter" << std::endl;
+	std::cout << "  3) Merge Filter" << std::endl;
+	std::cout << "  4) Flip Filter" << std::endl;
+	std::cout << "  5) Darken & Enlighten Filter" << std::endl;
+	std::cout << "  6) Rotate Image" << std::endl;
+	//std::cout << "  7) Detect Image Edges" << std::endl;
+	//std::cout << "  8) Enlarge Image" << std::endl;
+	//std::cout << "  9) Shrink Image" << std::endl;
+	//std::cout << "  a) Mirror 1/2 Image" << std::endl;
+	//std::cout << "  b) Shuffle Image" << std::endl;
+	//std::cout << "  c) Blur Image" << std::endl;
+	//std::cout << "  d) Crop Image" << std::endl;
+	//std::cout << "  e) Skew Image Right" << std::endl;
+	//std::cout << "  f) Skew Image Up" << std::endl;
+	std::cout << "  s) Save Image to file" << std::endl;
+	std::cout << "  0) Exit" << std::endl;
+	std::cout << "Your entry: ";
+	char option;
+	std::cin >> option;
+
+	return option;
+}
